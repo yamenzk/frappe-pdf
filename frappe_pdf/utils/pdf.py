324fdd01bcbccec916162c6bd5cf40c56a1358ee
yamenzk <98694759+yamenzk@users.noreply.github.com>
2024-03-24 17:57:47 +0400

    Update pdf.py
    Fixed indentation

--- frappe_pdf/utils/pdf.py
+++ frappe_pdf/utils/pdf.py
@@ -6,84 +6,81 @@ import frappe
 from frappe.utils import get_url
 
 URLS_NOT_HTTP_TAG_PATTERN = re.compile(
-	r'(href|src){1}([\s]*=[\s]*[\'"]?)((?!http)[^\'">]+)([\'"]?)'
-)  # href=/assets/...
+    r'(href|src){1}([\s]*=[\s]*[\'"]?)((?!http)[^\'">]+)([\'"]?)'
+) # href=/assets/...
 URL_NOT_HTTP_NOTATION_PATTERN = re.compile(
-	r'(:[\s]?url)(\([\'"]?)((?!http)[^\'">]+)([\'"]?\))'
-)  # background-image: url('/assets/...')
+    r'(:[\s]?url)(\([\'"]?)((?!http)[^\'">]+)([\'"]?\))'
+) # background-image: url('/assets/...')
 
 def scrub_urls(html: str) -> str:
-	return expand_relative_urls(html)
-
+    return expand_relative_urls(html)
 
 def expand_relative_urls(html: str) -> str:
-	# expand relative urls
-	url = get_url()
-	if url.endswith("/"):
-		url = url[:-1]
-	
-	URLS_HTTP_TAG_PATTERN = re.compile(
-		r'(href|src)([\s]*=[\s]*[\'"]?)((?:{0})[^\'">]+)([\'"]?)'.format(re.escape(url.replace("https://", "http://")))
-	)  # href='https://...
-	
-	URL_HTTP_NOTATION_PATTERN = re.compile(
-		r'(:[\s]?url)(\([\'"]?)((?:{0})[^\'">]+)([\'"]?\))'.format(re.escape(url.replace("https://", "http://")))
-	)  # background-image: url('/assets/...')	
+    # expand relative urls
+    url = get_url()
+    if url.endswith("/"):
+        url = url[:-1]
+    
+    URLS_HTTP_TAG_PATTERN = re.compile(
+        r'(href|src)([\s]*=[\s]*[\'"]?)((?:{0})[^\'">]+)([\'"]?)'.format(re.escape(url.replace("https://", "http://")))
+    ) # href='https://...
+    
+    URL_HTTP_NOTATION_PATTERN = re.compile(
+        r'(:[\s]?url)(\([\'"]?)((?:{0})[^\'">]+)([\'"]?\))'.format(re.escape(url.replace("https://", "http://")))
+    ) # background-image: url('/assets/...')    
 
+    def _expand_relative_urls(match):
+        to_expand = list(match.groups())
 
-	def _expand_relative_urls(match):
-		to_expand = list(match.groups())
+        if not to_expand[2].startswith(("mailto", "data:", "tel:")):
+            if not to_expand[2].startswith(url):
+                if not to_expand[2].startswith("/"):
+                    to_expand[2] = "/" + to_expand[2]
+                to_expand.insert(2, url)
+        
+        # add session id
+        if frappe.session and frappe.session.sid and hasattr(frappe.local, "request") and len(to_expand) > 2:
+            if "?" in to_expand[-2]:
+                to_expand[-2] += f"&sid={frappe.session.sid}"
+            else:
+                to_expand[-2] += f"?sid={frappe.session.sid}"
 
-		if not to_expand[2].startswith(("mailto", "data:", "tel:")):
-			if not to_expand[2].startswith(url):
-				if not to_expand[2].startswith("/"):
-					to_expand[2] = "/" + to_expand[2]
-				to_expand.insert(2, url)
-		
-		# add session id
-		if frappe.session and frappe.session.sid and hasattr(frappe.local, "request") and len(to_expand) > 2:
-			if "?" in to_expand[-2]:
-				to_expand[-2] += f"&sid={frappe.session.sid}"
-			else:
-				to_expand[-2] += f"?sid={frappe.session.sid}"
+        return "".join(to_expand)
 
-		return "".join(to_expand)
+    html = URLS_HTTP_TAG_PATTERN.sub(_expand_relative_urls, html)
+    html = URLS_NOT_HTTP_TAG_PATTERN.sub(_expand_relative_urls, html)
+    html = URL_NOT_HTTP_NOTATION_PATTERN.sub(_expand_relative_urls, html)
+    html = URL_HTTP_NOTATION_PATTERN.sub(_expand_relative_urls, html)
 
-	html = URLS_HTTP_TAG_PATTERN.sub(_expand_relative_urls, html)
-	html = URLS_NOT_HTTP_TAG_PATTERN.sub(_expand_relative_urls, html)
-	html = URL_NOT_HTTP_NOTATION_PATTERN.sub(_expand_relative_urls, html)
-	html = URL_HTTP_NOTATION_PATTERN.sub(_expand_relative_urls, html)
-
-	return html
+    return html
 
 def get_pdf(html, *a, **b):
-	pdf_file_path = f'/tmp/{frappe.generate_hash()}.pdf'
-	html = scrub_urls(html)
-	with tempfile.NamedTemporaryFile(mode="w+", suffix=f"{frappe.generate_hash()}.html", delete=True) as html_file:
-		html_file.write(html)
-		html_file.seek(0)
-		chrome_command = [
-			"/usr/bin/google-chrome",
-			"--headless",
-			"--disable-gpu",
-			"--no-pdf-header-footer",
-			"--run-all-compositor-stages-before-draw",
-			f"--print-to-pdf={pdf_file_path}",
-			html_file.name
-
-		]
-		subprocess.run(chrome_command, shell=False)
-		content = None
-		if not os.path.exists(pdf_file_path):
-		     	print(f"PDF file not found at {pdf_file_path}")
-			result = subprocess.run(['which', 'google-chrome'], capture_output=True, text=True)
-			if result.returncode == 0:
-			    print(f"Google Chrome is installed at: {result.stdout.strip()}")
-			else:
-			    print("Google Chrome is not found in the system's PATH.")
-		else:
-			with open(pdf_file_path, 'rb') as f:
-				content = f.read()
-			os.remove(pdf_file_path)
-	
-	return content
+    pdf_file_path = f'/tmp/{frappe.generate_hash()}.pdf'
+    html = scrub_urls(html)
+    with tempfile.NamedTemporaryFile(mode="w+", suffix=f"{frappe.generate_hash()}.html", delete=True) as html_file:
+        html_file.write(html)
+        html_file.seek(0)
+        chrome_command = [
+            "/usr/bin/google-chrome",
+            "--headless",
+            "--disable-gpu",
+            "--no-pdf-header-footer",
+            "--run-all-compositor-stages-before-draw",
+            f"--print-to-pdf={pdf_file_path}",
+            html_file.name
+        ]
+        subprocess.run(chrome_command, shell=False)
+        content = None
+        if not os.path.exists(pdf_file_path):
+            print(f"PDF file not found at {pdf_file_path}")
+            result = subprocess.run(['which', 'google-chrome'], capture_output=True, text=True)
+            if result.returncode == 0:
+                print(f"Google Chrome is installed at: {result.stdout.strip()}")
+            else:
+                print("Google Chrome is not found in the system's PATH.")
+        else:
+            with open(pdf_file_path, 'rb') as f:
+                content = f.read()
+            os.remove(pdf_file_path)
+    
+    return content
